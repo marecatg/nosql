bf0bdb0729f3839caef474ac7f5a7b76534b1801
Gaetan Marecat <gaetan.marecat@epsi.fr>
2017-04-25 11:16:32 +0200

    up

--- update_GMA.sh
+++ update_GMA.sh
@@ -3,10 +3,10 @@
 mongo <<EOF 
 use customersdb
 
-customersdb.createCollection('employeCollection', {size: 25});
+db.createCollection('employeCollection', {size: 25});
 
 for (var i = 0; i < 25; i++) {
-   customersdb.employeCollection.insert( { nom: "nameTest", prenom: "prenomTest", salaire: Math.floor(Math.random() * (15000 - 1000 + 1)) + 1000 });
+   db.employeCollection.insert( { nom: "nameTest", prenom: "prenomTest", salaire: Math.floor(Math.random() * (15000 - 1000 + 1)) + 1000 });
 }
 
 db.employeCollection.find().pretty();
