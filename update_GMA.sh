132ec7cc44e46539aba7ce0b231cc944174d5eee
Gaetan Marecat <gaetan.marecat@epsi.fr>
2017-04-25 11:40:33 +0200

    test remove

--- update_GMA.sh
+++ update_GMA.sh
@@ -9,6 +9,15 @@ for (var i = 0; i < 25; i++) {
    db.employeCollection.insert( { nom: "marecat " + i, prenom: "gaetan " + i, salaire: Math.floor(Math.random() * (15000 - 1000 + 1)) + 1000 });
 }
 
+var salaireMax;
+var i = 1;
+db.employeCollection.find().sort({salaire: 1}).forEach(function(employe) {
+    if (i >= 5) {
+        db.employeCollection.remove( employe );
+    }
+    i++;
+});
+
 db.employeCollection.find().pretty().sort({salaire: 1});
 
 EOF
